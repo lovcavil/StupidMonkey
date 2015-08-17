152ccea0472f5c735e0dffbe961c2e59bf80986b
lovcavil <lovcavil@hotmail.com>
2015-08-17 17:28:33 +0800

    tasktest

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var th = new System.Threading.Thread(testthread);
+            th.Start();
+            Console.ReadLine();
+            Console.ReadLine();
+        }
+
+        static void testthread()
+        {
+            Console.WriteLine("thread");
+        }
+    }
+}
